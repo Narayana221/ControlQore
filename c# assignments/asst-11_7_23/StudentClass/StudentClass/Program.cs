3d6d7df99081dfefd83020fbd7892e225d7d88cd
Naraya221 <narayanankunjunni@gmail.com>
2023-07-11 17:11:28 +0530

    completed

---
+++ c# assignments/asst-11_7_23/StudentClass/StudentClass/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student ob1 = new Student(1, "John", 85);
+            Student ob2 = new Student(2, "David", 74);
+            Student ob3 = new Student(3, "Thomas", 90);
+        }
+    }
+}
---
+++ c# assignments/asst-11_7_23/StudentClass/StudentClass/Student.cs
@@ -0,0 +1,57 @@
+﻿
+namespace StudentClass
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Mark { get; set; }
+        public char grade;
+        public char Grade
+        {
+            get
+            {
+                if (Mark < 70)
+                {
+                    grade = 'D';
+                }
+                else if (Mark <= 80)
+                {
+                    grade = 'C';
+                }
+                else if (Mark < 90)
+                {
+                    grade = 'B';
+                }
+                else
+                {
+                    grade = 'A';
+
+                }
+                return grade;
+
+            }
+        }
+
+        public Student(int id, string name, int mark)
+        {
+            Id = id;
+            Name = name;
+            Mark = mark;
+            Display();
+        }
+
+        public void Display()
+        {
+
+            Console.WriteLine($"Name is {Name}");
+            Console.WriteLine($"Mark is {Mark}");
+            Console.WriteLine($"Grade is {Grade}\n");
+
+
+        }
+
+
+
+    }
+}
