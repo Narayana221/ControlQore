d72b6e33d3eb4341d9ccecf30c84d78abe704c38
Naraya221 <narayanankunjunni@gmail.com>
2023-07-20 11:40:49 +0530

    completed

--- c# assignments/ASST-16_7_23/employee/EmployeeManagement/EmployeeManagement/Program.cs
+++ c# assignments/ASST-16_7_23/employee/EmployeeManagement/EmployeeManagement/Program.cs
@@ -5,21 +5,29 @@
         static void Main(string[] args)
         {
             double bonus = 0;
-            Console.WriteLine("Hello, World!");
-            Employee ob1 = new Employee(1, "narayanan", new DateTime(2023, 12, 23), "cs", 50000, 'p');
+            Employee ob1 = new Employee(1, "narayanan", new DateTime(2023, 12, 23), "cs", 50000, 'f');
             IBonusCalculator ib = null;
-            if ( ob1.JobType == 'p')
+
+
+            if (ob1.JobType == 'p')
             {
                 ib = new PermanentEmployeeBonusCalulator();
-                
+
             }
-            else if(ob1.JobType == 'c')
+            else if (ob1.JobType == 'c')
             {
                 ib = new ContractEmployeeBonusCalculator();
-               
+
             }
-            bonus = ib.CalculateBonus(ob1);
-            Console.WriteLine($"Bonus of {ob1.Name} with JobType as {ob1.JobType} is {bonus}");
+            if (ib != null)
+            {
+                bonus = ib.CalculateBonus(ob1);
+                Console.WriteLine($"Bonus of {ob1.Name} with JobType as {ob1.JobType} is {bonus}");
+            }
+            else 
+            { 
+                Console.WriteLine("error"); 
+            }
         }
     }
 }
