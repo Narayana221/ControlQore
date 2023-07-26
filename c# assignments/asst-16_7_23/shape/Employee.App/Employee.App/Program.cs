9b865985678e1f75843188af9f5823fcc62f0e95
Naraya221 <narayanankunjunni@gmail.com>
2023-07-26 18:44:51 +0530

    asst1

---
+++ c# assignments/asst-16_7_23/shape/Employee.App/Employee.App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Employee.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ c# assignments/asst-26_7_23/asst1/Employee.Model/Employee.Model/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Employee.Model
+{
+    public class Class1
+    {
+
+    }
+}
