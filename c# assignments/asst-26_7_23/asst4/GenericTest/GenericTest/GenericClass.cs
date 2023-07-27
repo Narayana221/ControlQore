48f0d3ba371e11ba071045e450bb3bad4de5d7ce
Naraya221 <narayanankunjunni@gmail.com>
2023-07-27 14:56:59 +0530

    complete

---
+++ c# assignments/asst-26_7_23/asst4/GenericTest/GenericTest/GenericClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTest
+{
+    internal class GenericClass<T> where T : System.IComparable<T>
+    {
+        public T Compare(T val1, T val2)
+        {
+            int ans = val1.CompareTo(val2);
+            if (ans > 0) return val1;
+            else return val2;
+
+        }
+    }
+    }
+}
---
+++ c# assignments/asst-26_7_23/asst4/GenericTest/GenericTest/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericClass<int> obj1 = new GenericClass<int>();
+            Console.WriteLine(obj1.Compare(2, 3));
+            GenericClass<double> obj2 = new GenericClass<double>();
+            Console.WriteLine(obj2.Compare(9.5, 3.2));
+
+        }
+    }
+}
