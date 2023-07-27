b12138ab19361657de611088234967acf597f824
Naraya221 <narayanankunjunni@gmail.com>
2023-07-27 15:02:28 +0530

    complete

---
+++ c# assignments/asst-26_7_23/asst5/ListExample/ListExample/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ListExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            var numbers = new List<int>();
+            int[] arr = { 1, 2, 3, 4, 5 };
+            for (int i =0; i < arr.Length; i++)
+            {
+                numbers.Add(arr[i]*2);
+            }
+
+            foreach (int i in numbers)
+                Console.WriteLine(i);
+        }
+
+    }
+}
