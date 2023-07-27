364fb559bb5b365e14d3f79281fc2a3a2b4d1ce7
Naraya221 <narayanankunjunni@gmail.com>
2023-07-27 11:21:57 +0530

    custom exception created

---
+++ c# assignments/asst-26_7_23/asst2/Custom/Custom/InvalidUserInputException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom
+{
+    internal class InvalidUserInputException:Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "User Input is Invalid";
+            }
+        }
+    }
+}
---
+++ c# assignments/asst-26_7_23/asst2/Custom/Custom/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Custom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the input character ");
+            char.TryParse(Console.ReadLine(), out char input);
+
+            try
+            {
+                switch (input)
+                {
+                    case 'c':
+                        Console.WriteLine("Cricket");
+                        break;
+                    case 'f':
+                        Console.WriteLine("Football");
+                        break;
+                    case 'h':
+                        Console.WriteLine("Hockey");
+                        break;
+                    case 't':
+                        Console.WriteLine("Tennis");
+                        break;
+                    case 'b':
+                        Console.WriteLine("Badminton");
+                        break;
+                    default:
+                        throw new InvalidUserInputException();
+
+                }
+            }
+            catch (InvalidUserInputException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
