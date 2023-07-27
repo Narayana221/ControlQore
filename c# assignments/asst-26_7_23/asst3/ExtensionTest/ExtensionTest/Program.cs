ac1be4f47ce5dc1b91dd1f2edabc326326d1473a
Naraya221 <narayanankunjunni@gmail.com>
2023-07-27 12:07:12 +0530

    completed

---
+++ c# assignments/asst-26_7_23/asst3/ExtensionTest/ExtensionTest/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ExtensionTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello World! Hello!";
+            Console.WriteLine("Occurrence:" + s.isWordCount("Hello"));
+        }
+    }
+}
---
+++ c# assignments/asst-26_7_23/asst3/ExtensionTest/ExtensionTest/StringExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionTest
+{
+    public static class StringExtension
+    {
+        public static int isWordCount(this string str, string pattern)
+        {
+            int count = 0;
+            int a = 0;
+            while ((a = str.IndexOf(pattern, a)) != -1)
+            {
+                a += pattern.Length;
+                count++;
+            }
+            return count;
+        }
+    }
+}
