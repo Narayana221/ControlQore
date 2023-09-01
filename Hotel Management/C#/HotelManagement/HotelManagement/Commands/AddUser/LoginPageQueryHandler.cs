af3f189143508742168f79c5a74479f59c469c23
Harik35 <harikrishnan.pm@controlqore.com>
2023-09-01 18:13:40 +0530

    Login page verification

--- Hotel Management/C#/HotelManagement/HotelManagement/Commands/AddUser/LoginPageQueryHandler.cs
+++ Hotel Management/C#/HotelManagement/HotelManagement/Commands/AddUser/LoginPageQueryHandler.cs
@@ -1,8 +1,9 @@
-﻿using HotelManagement.Repo.Context;
+﻿using HotelManagement.HotelManagement.Model.Model;
+using HotelManagement.Repo.Context;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
-namespace HotelManagement.Queries.LoginPageQuery
+namespace HotelManagement.Commands.AddUser
 {
     public class LoginPageQueryHandler : IRequestHandler<LoginPageQuery, string>
     {
@@ -15,8 +16,8 @@ namespace HotelManagement.Queries.LoginPageQuery
 
         public async Task<string> Handle(LoginPageQuery request, CancellationToken cancellationToken)
         {
-            
-            return await _context.Users.Where(x =>  x.UserName == request.username).Select(x => x.Name).FirstOrDefaultAsync(cancellationToken: cancellationToken);
+
+            return await _context.Users.Where(x => x.UserName == request.username && x.Password==request.password).Select(x=>x.Name).FirstOrDefaultAsync(cancellationToken: cancellationToken);
         }
     }
 }
