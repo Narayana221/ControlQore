2bce46a2f4e2c896ded35875cfcde34be07d48e1
Harik35 <harikrishnan.pm@controlqore.com>
2023-09-08 15:54:31 +0530

    startdate enddate

--- Hotel Management/C#/HotelManagement/HotelManagement/Commands/AddBooking/BookingCommandHandler.cs
+++ Hotel Management/C#/HotelManagement/HotelManagement/Commands/AddBooking/BookingCommandHandler.cs
@@ -35,7 +35,10 @@ namespace HotelManagement.Commands.AddBooking
             //List<int> Rooms = new List<int>();
            
             var Rooms = _dbContext.Room.Where(x => x.HotelId == request.HotelId && x.RoomTypeId== request.RoomTypeId && !_dbContext.BookedRoom.
-            Any(y => (request.StartDate >= y.StartDate && request.EndDate <= y.EndDate) && x.RoomId == y.RoomId)).
+            Any(y => (request.StartDate >= y.StartDate && request.EndDate <= y.EndDate) ||
+            (request.StartDate < y.StartDate && request.EndDate > y.StartDate) ||
+            (request.StartDate > y.StartDate && request.StartDate < y.EndDate)
+           && x.RoomId == y.RoomId)).
             Take(request.NoOfRooms).Select(x=> x.RoomId).ToList();
             var bookedRooms = Rooms.Select(x =>
             {
