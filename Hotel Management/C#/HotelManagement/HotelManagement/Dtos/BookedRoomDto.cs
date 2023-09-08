9c0115d51f0366339d289c2ff21bcd489194a570
Harik35 <harikrishnan.pm@controlqore.com>
2023-09-08 19:02:43 +0530

    bookedDetails

---
+++ Hotel Management/C#/HotelManagement/HotelManagement/Dtos/BookedRoomDto.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelManagement.Dtos
+{
+    public class BookedRoomDto
+    {
+       
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int RoomId { get; set; }
+        public string HotelName { get; set; }
+
+    }
+}
