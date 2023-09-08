983c770a58474725f3ffa5fb8e6f42b8aae1826d
Naraya221 <narayanankunjunni@gmail.com>
2023-09-08 14:47:39 +0530

    date condition updates

---
+++ Hotel Management/C#/HotelManagement/HotelManagement/Commands/AddRating/AddRatingCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace HotelManagement.Commands.AddRating
+{
+    public class AddRatingCommand : IRequest<bool>
+    {
+        public int Rating { get; set; }
+
+        public int BookingId { get; set; }
+    }
+}
---
+++ Hotel Management/C#/HotelManagement/HotelManagement/Commands/AddRating/AddRatingCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using HotelManagement.Commands.AddStudent;
+using HotelManagement.Repo.Context;
+using MediatR;
+using Microsoft.Identity.Client;
+
+namespace HotelManagement.Commands.AddRating
+{
+    public class AddRatingCommandHandler : IRequestHandler<AddRatingCommand, bool>
+    {
+
+        private readonly HotelManagementContext _dbContext;
+
+        public AddRatingCommandHandler(HotelManagementContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<bool> Handle(AddRatingCommand request, CancellationToken cancellationToken)
+        {
+            var x = _dbContext.Booking.Find(request.BookingId);
+            x.Rank = request.Rating;
+
+            var y = _dbContext.BookedRoom.Find(request.BookingId);
+            var z = y.Room.HotelId;
+
+            var a = _dbContext.Hotel.Find(z);
+            a.Rating = (a.Rating + request.Rating)/2;
+
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+}
