9f57e34add79174489fb0ff7bc5ddd06fd8e2f7a
Harik35 <harikrishnan.pm@controlqore.com>
2023-09-08 10:31:05 +0530

    UserBookingDetials

--- Hotel Management/C#/HotelManagement/HotelManagement/Queries/UserBookings/UserBookingQueryHandler.cs
+++ Hotel Management/C#/HotelManagement/HotelManagement/Queries/UserBookings/UserBookingQueryHandler.cs
@@ -1,6 +1,7 @@
 ﻿using HotelManagement.Dtos;
 using HotelManagement.Repo.Context;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection.Metadata.Ecma335;
 
 namespace HotelManagement.Queries.UserBookings
@@ -16,7 +17,20 @@ namespace HotelManagement.Queries.UserBookings
 
         public async Task<List<UserBookingsDto>> Handle(UserBookingQuery request, CancellationToken cancellationToken)
         {
-            return await _context.Booking.Where(x => x.UserId == request.Id).Select(x => x).ToList();
+            return await _context.BookedRoom.Where(x => x.Booking.UserId == request.Id)
+                .Select(x => new UserBookingsDto
+                {
+                    HotelName = x.Room.Hotel.Name,
+                    NoOfRooms = x.Booking.NoOfRooms,
+                    StartDate = x.Booking.StartDate,
+                    EndDate = x.Booking.EndDate,
+                    TotalPrice = x.Booking.TotalPrice,
+                    CheckOutStatus = x.CheckedOut,
+                    BookingId = x.Booking.BookingId
+
+
+                }).Distinct().ToListAsync();
         }
+           
     }
 }
